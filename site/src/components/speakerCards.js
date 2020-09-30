d9529fa9a21dd89d345b6c64c76215764c21c61d
Monica Powell <monica@aboutmonica.com>
2020-09-29 21:39:20 -0400

    update base speaker card

--- site/src/components/speakerCards.js
+++ site/src/components/speakerCards.js
@@ -5,14 +5,17 @@ export default function speakerCard({ speakers }) {
   return (
     <div className="flex items-center flex-wrap ">
       {speakers.map(({ node }) => (
-        <div className="w-1/3 p-2">
-          <img
-            className="rounded-full "
-            src={node.avatar}
-            alt={`Avatar of ${node.name}`}
-          />
-          {node.name}
-        </div>
+        // TODO: pass slug from node into the Link component
+        <Link to="" className="w-1/3 p-2" key={node.name}>
+          <div>
+            <img
+              className="rounded-full inline"
+              src={node.avatar}
+              alt={`Avatar of ${node.name}`}
+            />
+            {node.name}
+          </div>
+        </Link>
       ))}
     </div>
   );
